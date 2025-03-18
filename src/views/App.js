170b2d176666a561933405413d14e8f11560556c
cba_vargas <vargas@cybridge.jp>
2025-03-18 11:02:17 +0700

    update basic01

--- src/views/App.js
+++ src/views/App.js
@@ -1,20 +1,27 @@
 import logo from './logo.svg';
 import './App.scss';
-// import MyComponent from './example/MyComponent.js';
-import ListTodo from './todos/ListTodo.js';
 import { ToastContainer } from 'react-toastify'; //https://www.npmjs.com/package/react-toastify
 import 'react-toastify/dist/ReactToastify.css';
+import MyComponent from './example/MyComponent.js';
+import ListTodo from './todos/ListTodo.js';
+import Nav from './Nav/Nav.js';
+import Home from './example/Home.js';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <header className="App-header">
+      <Nav/>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Simple Todo App width react.js
-        </p>
-        {/* <MyComponent/> */}
-        <ListTodo/>
+        
+      <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/todo' element={<ListTodo/>} />
+      <Route path='/about' element={<MyComponent/>} />
+      </Routes>
+        
       </header>
       <ToastContainer
         position="top-right"
@@ -29,6 +36,7 @@ function App() {
         theme="light"
       />
     </div>
+    </BrowserRouter>
   );
 }
 export default App;
